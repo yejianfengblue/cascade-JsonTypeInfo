d26d54f0acf6c6c41187f20d26d6d92670a26379
Jian Feng Ye <yejianfengblue@163.com>
2020-12-24 07:04:15 +0800

    Compact code

--- src/main/java/A.java
+++ src/main/java/A.java
@@ -1,12 +1,8 @@
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
-@JsonTypeInfo(use = JsonTypeInfo.Id.NAME,
-              include = JsonTypeInfo.As.EXISTING_PROPERTY,
-              property = "type")
-@JsonSubTypes({
-                      @JsonSubTypes.Type(value = B.class, name = "B")
-              })
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.EXISTING_PROPERTY, property = "type")
+@JsonSubTypes({@JsonSubTypes.Type(value = B.class, name = "B")})
 public abstract class A {
 
     private final String type;
--- src/main/java/B.java
+++ src/main/java/B.java
@@ -3,12 +3,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
-@JsonTypeInfo(use = JsonTypeInfo.Id.NAME,
-              include = JsonTypeInfo.As.EXISTING_PROPERTY,
-              property = "type2")
-@JsonSubTypes({
-                      @JsonSubTypes.Type(value = C.class, name = "C")
-              })
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.EXISTING_PROPERTY, property = "type2")
+@JsonSubTypes({@JsonSubTypes.Type(value = C.class, name = "C")})
 public class B extends A {
 
     private final String type2;
